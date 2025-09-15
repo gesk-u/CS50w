96e629d3b8c811c790efcdb9f4e66835ccbaec48
gesk-u <anastasiiatkach10@gmail.com>
2025-09-15 19:12:19 +0300

    2

--- week_2/lecture/hello/urls.py
+++ week_2/lecture/hello/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     #1.Empty string - no additional arguments(nothing at the end of the route) 
     #2. what view.py should be rendered when this url is visited "index" the name of the function in views.py 
     #3. (optional) provide a path vith a string name
-    path("", views.index, name="index") 
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("david", views.david, name="david")
 ]
---
+++ week_2/lecture/hello/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world!")  # HttpResponse is a special class created by Django
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
