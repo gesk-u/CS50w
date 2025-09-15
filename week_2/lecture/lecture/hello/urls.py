c0646bbcf0f2b8e31c41f96ca03636da6abd67ca
gesk-u <anastasiiatkach10@gmail.com>
2025-09-15 11:48:28 +0300

    1

---
+++ week_2/lecture/lecture/hello/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views # "." because views.py and urls.py are located in the same directory
+
+urlpatterns = [
+    #1.Empty string - no additional arguments(nothing at the end of the route) 
+    #2. what view.py should be rendered when this url is visited "index" the name of the function in views.py 
+    #3. (optional) provide a path vith a string name
+    path("", views.index, name="index") 
+]
