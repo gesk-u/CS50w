bc93a5eb71cda4e0cd4fb980b8b20f30c2d49e68
gesk-u <anastasiiatkach10@gmail.com>
2025-09-19 21:03:01 +0300

    3

--- week_2/lecture/hello/views.py
+++ week_2/lecture/hello/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")  # HttpResponse is a special class created by Django
+    return render(request, "hello/index.html")  # HttpResponse is a special class created by Django
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
@@ -13,4 +13,6 @@ def david(request):
     return HttpResponse("Hello, David")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
