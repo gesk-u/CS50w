5320542ad98980c8eb5c102adf6ec8ac99961952
gesk-u <anastasiiatkach10@gmail.com>
2025-09-20 16:14:14 +0300

    20.09

--- week_2/project1/wiki/encyclopedia/views.py
+++ week_2/project1/wiki/encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 from . import util
+from django.db import models
+import markdown2
+
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    html =  markdown2.markdown(util.get_entry(title))
+    return render(request, "encyclopedia/entry.html", {
+        "entry_title": title,
+        "entry_content": html
     })
 
